14503004f3cbd80d1a895b25abe1f1c15c61dfd4
ACuteWoof <sutharshanvithushan@gmail.com>
2024-08-17 21:47:59 +0530

    pretiter

--- render.sh
+++ render.sh
@@ -4,4 +4,4 @@ cat src/top.html >> public/index.html
 cat src/books.html >> public/index.html
 cat src/btm.html >> public/index.html
 
-prettier -w public/index.html
+yarn run prettier -w public/index.html
