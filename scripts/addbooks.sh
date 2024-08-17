db799b3bb0c038f19b159505bc2b40d21d14cda6
ACuteWoof <sutharshanvithushan@gmail.com>
2024-08-17 22:26:34 +0530

    books

--- scripts/addbooks.sh
+++ scripts/addbooks.sh
@@ -2,7 +2,8 @@
 
 cd "${0%/*}"
 
-echo "" > ../extra/books.html
+echo "" > ../extra/books.html || touch ../extra/books.html
+
 while IFS="|" read -r TITLE AUTHOR SUBNOTE DESCRIPTION IMAGE LULU PRICE PDF; do
 	TEXT="<!--- $TITLE ---><div class='entry'><div class='entrycontent'><img class='w-30' src='$IMAGE' alt='$TITLE $AUTHOR' /><div class='flex flex-col gap-2'><div class='flex flex-col gap-1'><h2>$TITLE - $AUTHOR</h2><span class='subnote'>$SUBNOTE</span></div><p>$DESCRIPTION</p></div></div><div class='buttons'><a href='$LULU' target='_blank' class='text-blue-200'>Buy on Lulu - \$$PRICE</a><a href='$PDF' target='_blank'>View PDF</a></div></div>"
 	echo $TEXT >> ../extra/books.html
