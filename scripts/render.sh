afe4b40adab87e5fd65d43f575b38d486f99763f
ACuteWoof <sutharshanvithushan@gmail.com>
2024-08-17 22:23:23 +0530

    shit

--- scripts/render.sh
+++ scripts/render.sh
@@ -2,10 +2,12 @@
 
 cd "${0%/*}"
 
-echo "" > ../public/index.html
+echo "" > ../public/index.html || touch ../public/index.html
 
+cp ../src/public/* ../public/.
 cat ../src/top.html >> ../public/index.html
 cat ../extra/books.html >> ../public/index.html
 cat ../src/btm.html >> ../public/index.html
 
 yarn run prettier -w  ${PWD%/*}/public/index.html
+yarn build
