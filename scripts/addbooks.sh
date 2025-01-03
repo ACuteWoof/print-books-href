c29cf5e517eead07077e7929cade13f61ac7340b
Vithushan <sutharshanvithushan@gmail.com>
2025-01-04 01:34:18 +0530

    fix: link to fs server instead of g drive

--- scripts/addbooks.sh
+++ scripts/addbooks.sh
@@ -6,7 +6,7 @@ cd "${0%/*}"
 
 echo "" > ../extra/books.html || touch ../extra/books.html
 
-while IFS="|" read -r TITLE AUTHOR SUBNOTE DESCRIPTION IMAGE LULU PRICE PDF; do
-	TEXT="<!--- $TITLE ---><div class='entry'><div class='entrycontent'><img class='w-30' src='$IMAGE' alt='$TITLE $AUTHOR' /><div class='flex flex-col gap-2'><div class='flex flex-col gap-1'><h2>$TITLE - $AUTHOR</h2><span class='subnote'>$SUBNOTE</span></div><p>$DESCRIPTION</p></div></div><div class='buttons'><a href='$LULU' target='_blank' class='link-text'>Buy on Lulu - \$$PRICE</a><a href='$PDF' target='_blank'>View PDF</a></div></div>"
+while IFS="|" read -r TITLE AUTHOR SUBNOTE DESCRIPTION IMAGE LULU PRICE; do
+	TEXT="<!--- $TITLE ---><div class='entry'><div class='entrycontent'><img class='w-30' src='$IMAGE' alt='$TITLE $AUTHOR' /><div class='flex flex-col gap-2'><div class='flex flex-col gap-1'><h2>$TITLE - $AUTHOR</h2><span class='subnote'>$SUBNOTE</span></div><p>$DESCRIPTION</p></div></div><div class='buttons'><a href='$LULU' target='_blank' class='link-text'>Buy on Lulu - \$$PRICE</a></div></div>"
 	echo $TEXT >> ../extra/books.html
 done < ../src/books.txt
